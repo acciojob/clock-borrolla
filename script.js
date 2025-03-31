52005f5498206a05da5572f571e4ce449c418291
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-31 20:39:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function updateDateTime(){
+	const now = new Date();
+
+	const dateOptions = {
+		year : 'numeric',
+		month: '2-digit',
+		day : '2-digit',
+	};
+
+	const timeOptions = {
+		hour: '2-digit',
+		minute : '2-digit',
+		second: '2-digit',
+		hour12: true,
+	};
+
+
+	const formatteDate = now.toLocaleDateString('en-US', dateOptions);
+    const formatteDate = now.toLocaleTimeString('en-US', timeOptions);
+
+	document.getElementById('timer').textContent = `${formattedDate}, ${formatteTime}`;
+
+	setInterval(updateDateTime, 1000);
+
+	updateDateTime();
+	
+
+
+
+
+	
